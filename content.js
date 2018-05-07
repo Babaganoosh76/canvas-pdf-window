b3b191d885b4c23d37e65d2b55fca3c2a128ae18
Adam Miller <babaganoosh@Adams-MacBook-Pro.local>
2018-05-06 19:30:30 -0700

    completed extension

---
+++ content.js
@@ -0,0 +1,20 @@
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+	var iframe = document.getElementsByClassName('ef-file-preview-frame')[0]
+	if (!iframe) {
+		return alert('Please open a file before clicking the extension')
+	}
+	var request = new XMLHttpRequest()
+	request.open("GET", iframe.src, true)
+	request.send(null)
+	request.onreadystatechange = function() {
+		if (request.readyState == 4) {
+			url = request.responseText.match('"url":"(.*)"')[1]
+			if (!url) {
+				return alert('Could not find a valid file url.')
+			} else {
+				console.log('https://canvadocs.instructure.com'+url)
+				window.open('https://canvadocs.instructure.com'+url)
+			}
+		}
+	}
+})
